f3398f6791426b2e8293950dceaf840e2829aa54
Kai Davenport <kaiyadavenport@gmail.com>
2015-01-20 23:16:38 +0000

    test passing for entrypoint remapping

--- actions/create.js
+++ actions/create.js
@@ -9,5 +9,9 @@
 */
 
 module.exports = function(req, callback){
+  var cmd = [req.Body.Entrypoint, req.Body.Cmd].join(' ')
+  req.Body.Entrypoint = '/home/weavetools/wait-for-weave'
+  req.Body.Cmd = cmd
+  req.Body.HostConfig.VolumesFrom.push('weavetools:ro')
   callback(null, 200, req)
 }
