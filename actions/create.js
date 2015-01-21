301bed7fd807754c7a7d72c439a36345f0e3988c
Kai Davenport <kaiyadavenport@gmail.com>
2015-01-21 10:06:06 +0000

    for create action use constants for volume and binary path

--- actions/create.js
+++ actions/create.js
@@ -8,10 +8,13 @@
   
 */
 
+const WAIT_FOR_WEAVE_PATH = '/home/weavetools/wait-for-weave'
+const WAIT_FOR_WEAVE_VOLUME = 'weavetools:ro'
+
 module.exports = function(req, callback){
   var cmd = [req.Body.Entrypoint, req.Body.Cmd].join(' ')
-  req.Body.Entrypoint = '/home/weavetools/wait-for-weave'
+  req.Body.Entrypoint = WAIT_FOR_WEAVE_PATH
   req.Body.Cmd = cmd
-  req.Body.HostConfig.VolumesFrom.push('weavetools:ro')
+  req.Body.HostConfig.VolumesFrom.push(WAIT_FOR_WEAVE_VOLUME)
   callback(null, 200, req)
 }
