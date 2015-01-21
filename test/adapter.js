b0df67ee70bb8a2443ac646cb67f3f4d24400ceb
Kai Davenport <kaiyadavenport@gmail.com>
2015-01-21 10:54:39 +0000

    test for new request format

--- test/adapter.js
+++ test/adapter.js
@@ -12,17 +12,23 @@ tape('the Adapter should not alter a GET request', function(t){
   var adapter = Adapter()
 
   var req = {
-    Method:'GET',
-    Request:'/containers/json'
+    PowerstripProtocolVersion: 1,
+    Type: "pre-hook",
+    ClientRequest: {
+      Method:'GET',
+      Request:'/v1.16/containers/json'
+    }
   }
 
   adapter(req, function(err, code, response){
     t.error(err, 'there is no error')
     
     t.deepEqual(response, {
-      Method:'GET',
-      Request:'/containers/json',
-      Body:undefined
+      PowerstripProtocolVersion: 1,
+      ModifiedClientRequest: {
+        Method:'GET',
+        Request:'/v1.16/containers/json'
+      }
     }, 'same packet for GET')
 
     t.equal(code, 200, '200 status code for GET')
