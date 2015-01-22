9d33188781f7314c1fddbb8a3df875e6a004c12e
Kai Davenport <kaiyadavenport@gmail.com>
2015-01-22 00:26:20 +0000

    test passing for extracting the WEAVE_CIDR env variable

--- utils.js
+++ utils.js
@@ -1,7 +1,33 @@
 module.exports = {
+  /*
+  
+    grab a container id from a url like:
+
+      /v1.16/containers/123456/start
+    
+  */
   extractStartID:function(url){
     var id = url.replace(/^.*?\/containers\//, '')
     id = id.replace(/\/.*$/, '')
     return id
+  },
+
+
+  /*
+  
+    extract the WEAVE_CIDR environment variable
+    from the JSON response from `docker inspect 1234`
+    
+  */
+  extractWeaveEnv:function(packet){
+    var envVars = packet[0].Config.Env || []
+
+    var weaveVars = envVars.filter(function(envVar){
+      return envVar.indexOf('WEAVE_CIDR=')==0
+    }).map(function(envVar){
+      return envVar.split('=')[1]
+    })
+
+    return weaveVars[0]
   }
 }
