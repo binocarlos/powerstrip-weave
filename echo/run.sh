984fbd702d55b94a421982a7585129656923c043
Kai Davenport <kaiyadavenport@gmail.com>
2015-01-23 12:35:54 +0000

    echo script to time how long weave takes to connect

--- echo/run.sh
+++ echo/run.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
+counter=0
+max=100
+
+function tickcounter() {
+  counter=$((counter+1))
+  echo $counter
+  if (( counter >= max )); then
+    quitcounter
+  fi
+  sleep .1
+}
+
+function quitcounter() {
+  let ms=$counter*100
+  echo "Network did not connect after $ms ms";
+  exit 1;
+}
+
+function finishcounter() {
+  let ms=$counter*100
+  echo "Network took: $ms ms to connect"
+}
 
 while ! grep -q ^1$ /sys/class/net/ethwe/carrier 2>/dev/null
-do echo "no network" && sleep .1
+do tickcounter
 done
-echo "have network now"
+finishcounter
